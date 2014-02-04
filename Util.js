de63b3e4a392e5466147595415896b7e5469b6b5
awillis@fastmail.fm <andrewwillis@venda.com>
2014-02-04 17:16:12 +0000

    Updated

--- Util.js
+++ Util.js
@@ -8,7 +8,7 @@
      * @return {[type]} [description]
      */
     (function () {
-      function CustomEvent ( event, params ) {
+      function CustomEvent(event, params) {
         params = params || { bubbles: false, cancelable: false, detail: undefined };
         var evt = document.createEvent( 'CustomEvent' );
         evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
